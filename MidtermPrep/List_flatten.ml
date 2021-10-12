b41c7d12607f80230fbcc4c7242c809f8a6dcb1d
hexaquarks <seseriusca@gmail.com>
2021-10-12 16:08:48 -0400

    Flatteing a tree

---
+++ MidtermPrep/List_flatten.ml
@@ -0,0 +1,9 @@
+type 'a node = 
+| One of 'a 
+| Many of 'a node list ;;
+let flatten l = 
+  let rec aux acc l = match l with 
+  | [] -> acc
+  | One x::t -> aux (x::acc) t
+  | Many x::t -> aux (aux acc x) t
+  in List.rev(aux [] l);;
